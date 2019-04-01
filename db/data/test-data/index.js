e323c263ad3f0d956337d0adc344ce50a1a12fc7
theCodeFox <akkitta@live.co.uk>
2019-04-01 11:26:26 +0100

    surgery test data started

--- db/data/test-data/index.js
+++
@@ -1,8 +0,0 @@
-const test = require('.');
-const development = require('./development-data');
-
-const env = process.env.NODE_ENV || 'development';
-
-const data = { test, development, production: development };
-
-module.exports = data[env];
---
+++ db/data/test-data/surgeries.ts
@@ -0,0 +1,12 @@
+class Surgery {
+  constructor(
+    gp_id: number,
+    surgery_name: string,
+    surgery_username: string,
+    surgery_password: string,
+    surgery_id: number,
+    surgery_address: string,
+  ) {
+
+  }
+}
