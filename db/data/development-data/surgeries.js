cb8c380bc1df85dfea6a6dedd77e9357cad57105
theCodeFox <akkitta@live.co.uk>
2019-04-01 19:52:00 +0100

    dev surgery data compiled

--- db/data/development-data/surgeries.js
+++ db/data/development-data/surgeries.js
@@ -5,4 +5,6 @@ var Surgery = /** @class */ (function () {
     }
     return Surgery;
 }());
-exports.surgery1 = new Surgery('the ranch surgery', 'ranch_surgery', 'password123', 1, 'the Ranch/shed Hill/chortlon/M21 7BB');
+var surgery1 = new Surgery('the ranch surgery', 'ranch_surgery', 'password123', 1, 'the Ranch/shed Hill/chortlon/M21 7BB');
+var surgery2 = new Surgery('lions arch surgery', 'lionsArch', 'cats4ever', 2, '101 lions arch/divinitys reach/prestwich/M8 2SP');
+exports.surgeries = [surgery1, surgery2];
--- db/data/development-data/surgeries.ts
+++ db/data/development-data/surgeries.ts
@@ -10,10 +10,20 @@ class Surgery {
   }
 }
 
-export const surgery1 = new Surgery(
+const surgery1 = new Surgery(
   'the ranch surgery',
   'ranch_surgery',
   'password123',
   1,
   'the Ranch/shed Hill/chortlon/M21 7BB'
 );
+
+const surgery2 = new Surgery(
+  'lions arch surgery',
+  'lionsArch',
+  'cats4ever',
+  2,
+  '101 lions arch/divinitys reach/prestwich/M8 2SP'
+);
+
+export let surgeries = [surgery1, surgery2];
