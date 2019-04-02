4715b4cd527dd8916f306d32387584019a91725f
Esmé O`Brien <eobrien874@gmail.com>
2019-04-02 12:44:59 +0100

    gps data now in correct format

--- db/data/test-data/gps.ts
+++ db/data/test-data/gps.ts
@@ -1,28 +1,20 @@
-class GP {
-	surgery_id:number;
-	gp_name: string;
-	constructor(
-		surgery_id:number,
-		gp_name: string,
-	) {
-		this.surgery_id = surgery_id;
-		this.gp_name = gp_name;
-	}
-}
+module.exports = [
+	{
+		surgery_id: 1,
+		gp_name:'Doctor Krieger'
+	},
+	{
+		surgery_id: 1,
+		gp_name:'Madame Pomfrey'
+	},
+	{
+		surgery_id: 1,
+		gp_name:'Doctor Zoidberg'
+	},
+]
+	
+	
 
-const gp1 = new GP(
-	1,
-	'Doctor Krieger'
-)
 
-const gp2 = new GP(
-	1,
-	'Madame Pomfrey'
-)
 
-const gp3 = new GP(
-	1,
-	'Doctor Zoidberg'
-)
 
-export let gps = [gp1, gp2, gp3]
