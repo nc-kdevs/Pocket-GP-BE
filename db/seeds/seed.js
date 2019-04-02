251341b690a8dafb4f4664092819f7359258708e
theCodeFox <akkitta@live.co.uk>
2019-04-02 16:37:00 +0100

    formatGpData util working and tested

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,4 +1,7 @@
+"use strict";
+exports.__esModule = true;
 var _a = require('../data/index.js'), patientData = _a.patientData, gpData = _a.gpData, ailmentData = _a.ailmentData, surgeryData = _a.surgeryData;
+var utils_1 = require("../utils/utils");
 exports.seed = function (knex, Promise) { return knex.migrate
     .rollback()
     .then(function () { return knex.migrate.latest(); })
@@ -6,13 +9,16 @@ exports.seed = function (knex, Promise) { return knex.migrate
     return knex('surgeries').insert(surgeryData).returning('*');
 })
     .then(function (surgeries) {
-    console.log(surgeries, '<-- surgeries');
-    return knex('patients').insert(patientData).returning('*');
+    // console.log(surgeries, '<-- surgeries')
+    var formattedGpData = utils_1.formatGpData(gpData);
+    console.log(formattedGpData);
+    return knex('patients').insert(formattedGpData).returning('*');
 })
-    .then(function (gps) {
-    console.log(gps, '<--gps');
-    return knex('ailments').insert(ailmentData).returning('*');
-})
-    .then(function (ailments) {
-    console.log(ailments, '<-- ailments');
-}); };
+    .then(console.log); };
+// .then((gps) => {
+//   console.log(gps, '<--gps')
+//   return knex('ailments').insert(ailmentData).returning('*')
+// })
+// .then((ailments) => {
+//   console.log(ailments, '<-- ailments')
+// })
--- db/seeds/seed.ts
+++ db/seeds/seed.ts
@@ -1,6 +1,7 @@
 const {
   patientData, gpData, ailmentData, surgeryData,
 } = require('../data/index.js');
+import { formatGpData } from '../utils/utils';
 
 exports.seed = (knex, Promise) => knex.migrate
   .rollback()
@@ -9,16 +10,19 @@ exports.seed = (knex, Promise) => knex.migrate
     return knex('surgeries').insert(surgeryData).returning('*')
   })
   .then((surgeries) => {
-    console.log(surgeries, '<-- surgeries')
-    return knex('patients').insert(patientData).returning('*')
+    // console.log(surgeries, '<-- surgeries')
+    const formattedGpData = formatGpData(gpData);
+    console.log(formattedGpData)
+    return knex('patients').insert(formattedGpData).returning('*')
   })
-  .then((gps) => {
-    console.log(gps, '<--gps')
-    return knex('ailments').insert(ailmentData).returning('*')
-  })
-  .then((ailments) => {
-    console.log(ailments, '<-- ailments')
-  })
+  .then(console.log)
+  // .then((gps) => {
+  //   console.log(gps, '<--gps')
+  //   return knex('ailments').insert(ailmentData).returning('*')
+  // })
+  // .then((ailments) => {
+  //   console.log(ailments, '<-- ailments')
+  // })
 
 
 
