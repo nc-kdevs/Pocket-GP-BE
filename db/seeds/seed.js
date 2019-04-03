1c72fee63dc070297c053e41a4a9012442adcdcc
Esmé O`Brien <eobrien874@gmail.com>
2019-04-03 14:41:18 +0100

    merge setup

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,19 +2,21 @@
 exports.__esModule = true;
 var _a = require('../data/index.js'), patientData = _a.patientData, gpData = _a.gpData, ailmentData = _a.ailmentData, surgeryData = _a.surgeryData;
 var utils_1 = require("../utils/utils");
-exports.seed = function (knex, Promise) { return knex.migrate
-    .rollback()
-    .then(function () { return knex.migrate.latest(); })
-    .then(function () {
-    return knex('surgeries').insert(surgeryData).returning('*');
-})
-    .then(function (surgeries) {
-    // console.log(surgeries, '<-- surgeries')
-    var formattedGpData = utils_1.formatGpData(gpData);
-    console.log(formattedGpData);
-    return knex('patients').insert(formattedGpData).returning('*');
-})
-    .then(console.log); };
+exports.seed = function (knex, Promise) {
+    return knex.migrate
+        .rollback()
+        .then(function () { return knex.migrate.latest(); })
+        .then(function () {
+            return knex('surgeries').insert(surgeryData).returning('*');
+        })
+        .then(function (surgeries) {
+            // console.log(surgeries, '<-- surgeries')
+            var formattedGpData = utils_1.formatGpData(gpData);
+            console.log(formattedGpData);
+            return knex('patients').insert(formattedGpData).returning('*');
+        })
+        .then(console.log);
+};
 // .then((gps) => {
 //   console.log(gps, '<--gps')
 //   return knex('ailments').insert(ailmentData).returning('*')
