d9abe715d2125b51ae7a647108b298d4a410b328
theCodeFox <akkitta@live.co.uk>
2019-04-04 13:07:36 +0100

    get gp returns all gps

--- controllers/gps.js
+++ controllers/gps.js
@@ -8,11 +8,17 @@ exports.getGps = function (req, res, next) {
         conditions['gps.surgery_id'] = surgery;
     gps_js_1.fetchGps(conditions)
         .then(function (gps) {
-        console.log(gps);
         res.status(200).send({ gps: gps });
     })["catch"](next);
 };
 exports.postGp = function (req, res, next) {
+    var gp = req.body;
+    gps_js_1.addGp(gp)
+        .then(function (_a) {
+        var newGp = _a[0];
+        console.log(newGp);
+        res.status(201).send({ gp: newGp });
+    })["catch"](next);
 };
 exports.getGpByID = function (req, res, next) {
 };
--- controllers/gps.ts
+++ controllers/gps.ts
@@ -1,4 +1,4 @@
-import { fetchGps } from '../models/gps.js';
+import { fetchGps, addGp } from '../models/gps.js';
 import { Request, Response, NextFunction } from 'express';
 
 export const getGps = (req: Request, res: Response, next: NextFunction) => {
@@ -7,14 +7,19 @@ export const getGps = (req: Request, res: Response, next: NextFunction) => {
   if (surgery) conditions['gps.surgery_id'] = surgery;
   fetchGps(conditions)
     .then((gps: object[]) => {
-      console.log(gps)
       res.status(200).send({ gps })
     })
     .catch(next)
 }
 
 export const postGp = (req: Request, res: Response, next: NextFunction) => {
-
+  const gp: object = req.body;
+  addGp(gp)
+    .then(([newGp]) => {
+      console.log(newGp)
+      res.status(201).send({ gp: newGp })
+    })
+  .catch(next)
 }
 
 export const getGpByID = (req: Request, res: Response, next: NextFunction) => {
