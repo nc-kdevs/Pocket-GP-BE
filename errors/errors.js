064cbf775fb6fee6d909fcffd0ee706c173fb66a
theCodeFox <akkitta@live.co.uk>
2019-04-04 10:07:05 +0100

    error handling bug fix

--- errors/errors.js
+++ errors/errors.js
@@ -1,47 +1,45 @@
 "use strict";
 exports.__esModule = true;
 exports.handle400 = function (err, req, res, next) {
+    var _a;
     console.log(err);
     var code = err.code;
-    var errorCodes400 = {
-        23502: 'Invalid Request'
-    };
-    if (errorCodes400[code] || err.status === 400) {
-        res.status(400).send({ message: errorCodes400[code] || err.msg });
+    var errorCodes400 = (_a = {},
+        _a[code] = 'Invalid Request',
+        _a);
+    if (Object.keys(errorCodes400).includes('23502' || '42601' || '22P02')) {
+        res.status(400).send({ message: errorCodes400[code] });
     }
     else
         next(err);
 };
 exports.handle404 = function (err, req, res, next) {
+    var _a;
     var code = err.code;
-    var errorCodes404 = {};
-    if (errorCodes404[code] || err.status === 404) {
-        res.status(404).send({ message: errorCodes404[code] || err.msg });
+    var errorCodes404 = (_a = {},
+        _a[code] = 'Not Found',
+        _a);
+    if (Object.keys(errorCodes404).includes('22001' || 'ENOENT' || '42703')) {
+        res.status(404).send({ message: errorCodes404[code] });
     }
     else
         next(err);
 };
-exports.handle405 = function (err, req, res, next) {
-    res.status(405).send({ msg: 'Method Not Allowed' });
-};
 exports.handle422 = function (err, req, res, next) {
+    var _a;
     var code = err.code;
-    var errorCodes422 = {
-        23503: 'Unique Key Violation!. Request cannot be proccessed',
-        23505: 'Unique Key Violation!. Request cannot be proccessed'
-    };
-    if (errorCodes422[code] || err.status === 422) {
-        res.status(422).send({ message: errorCodes422[code] || err.msg });
+    var errorCodes422 = (_a = {},
+        _a[code] = 'Unique Key Violation!. Request cannot be proccessed',
+        _a);
+    if (Object.keys(errorCodes422).includes('23503' || '23503')) {
+        res.status(422).send({ message: errorCodes422[code] });
     }
     else
         next(err);
 };
 exports.handle500 = function (err, req, res, next) {
-    var code = err.code;
-    var errorCodes500 = {
-        42703: 'Property does not Exist - Internal Server Error'
-    };
-    if (errorCodes500[code]) {
-        res.status(500).send({ message: errorCodes500[code] });
-    }
+    res.status(422).send({ message: 'Property does not Exist - Internal Server Error' });
+};
+exports.handle405 = function (err, req, res, next) {
+    res.status(405).send({ msg: 'Method Not Allowed' });
 };
--- errors/errors.ts
+++ errors/errors.ts
@@ -4,43 +4,37 @@ export const handle400 = (err: any, req: Request, res: Response, next: NextFunct
   console.log(err)
   const { code } = err;
   const errorCodes400 = {
-    23502: 'Invalid Request'
+    [code]: 'Invalid Request'
   };
-  if (errorCodes400[code] || err.status === 400) {
-    res.status(400).send({ message: errorCodes400[code] || err.msg });
+  if (Object.keys(errorCodes400).includes('23502' || '42601' || '22P02')) {
+    res.status(400).send({ message: errorCodes400[code] });
   } else next(err);
-};
+}
 
 export const handle404 = (err: any, req: Request, res: Response, next: NextFunction) => {
   const { code } = err;
   const errorCodes404 = {
+    [code]: 'Not Found'
   };
-  if (errorCodes404[code] || err.status === 404) {
-    res.status(404).send({ message: errorCodes404[code] || err.msg });
+  if (Object.keys(errorCodes404).includes('22001' || 'ENOENT' || '42703')) {
+    res.status(404).send({ message: errorCodes404[code] });
   } else next(err);
-};
-
-export const handle405 = (err: any, req: Request, res: Response, next: NextFunction) => {
-  res.status(405).send({ msg: 'Method Not Allowed' });
 };
 
 export const handle422 = (err: any, req: Request, res: Response, next: NextFunction) => {
   const { code } = err;
   const errorCodes422 = {
-    23503: 'Unique Key Violation!. Request cannot be proccessed',
-    23505: 'Unique Key Violation!. Request cannot be proccessed',
+    [code]: 'Unique Key Violation!. Request cannot be proccessed',
   };
-  if (errorCodes422[code] || err.status === 422) {
-    res.status(422).send({ message: errorCodes422[code] || err.msg });
+  if (Object.keys(errorCodes422).includes('23503' || '23503')) {
+    res.status(422).send({ message: errorCodes422[code] });
   } else next(err);
 };
 
 export const handle500 = (err: any, req: Request, res: Response, next: NextFunction) => {
-  const { code } = err;
-  const errorCodes500 = {
-    42703: 'Property does not Exist - Internal Server Error',
-  };
-  if (errorCodes500[code]) {
-    res.status(500).send({ message: errorCodes500[code] });
-  }
+  res.status(422).send({ message: 'Property does not Exist - Internal Server Error' });
+};
+
+export const handle405 = (err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(405).send({ msg: 'Method Not Allowed' });
 };
