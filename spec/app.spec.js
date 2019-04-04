47df8700953971491cf10b1f641f4dad027a33af
theCodeFox <akkitta@live.co.uk>
2019-04-04 10:55:18 +0100

    test bug fix

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,23 +1,19 @@
-"use strict";
-exports.__esModule = true;
 process.env.NODE_ENV = 'test';
 var expect = require('chai').expect;
 var supertest = require('supertest');
 var connection = require('../db/connection.js');
 var appTest = require('../app');
+// import { Request, Response } from 'express';
 var request = supertest(appTest);
 // console.log(appTest)
 describe('/', function () {
-    beforeEach(function () {
-        console.log(appTest.listeners());
-        return connection.seed.run();
-    });
+    beforeEach(function () { return connection.seed.run(); });
     after(function () { return connection.destroy(); });
     describe('/gps', function () {
     });
     describe('/patients', function () {
     });
-    describe.only('/surgeries', function () {
+    describe('/surgeries', function () {
         it('GET 200 returns a list of all the surgeries', function () {
             return request.get('/api/surgeries').expect(200).then(function (res) {
                 expect(res.body.surgeries[0]).to.contain.keys('surgery_id', 'surgery_name', 'surgery_username', 'surgery_password', 'surgery_address');
@@ -25,18 +21,29 @@ describe('/', function () {
         });
         it('POST 201 return a posted surgery', function () {
             var newSurgery = { surgery_name: 'New Surgery', surgery_username: 'newSurgery', surgery_password: 'newsurgery21', surgery_address: '121 new suregery street m21 3th' };
-            return request.post('/api/surgeries').send(newSurgery).expect(201).then(function (res) {
+            console.log(appTest.listeners(), '<-- listeners during 2');
+            return request
+                .post('/api/surgeries')
+                .send(newSurgery)
+                .expect(201)
+                .then(function (res) {
                 expect(res.body.surgery).to.contain.keys('surgery_id', 'surgery_name', 'surgery_username', 'surgery_password', 'surgery_address');
             });
         });
-        it('POST / responds with status 400 - Invalid Body', function () {
+        it.only('POST / responds with status 400 - Invalid Body', function () {
             var newSurgery = { surgery_username: 'newSurgery', surgery_password: 'newsurgery21', surgery_address: '121 new suregery street m21 3th' };
-            return request.post('/api/surgeries').send(newSurgery).expect(400).then(function (res) {
+            return request
+                .post('/api/surgeries')
+                .send(newSurgery)
+                .expect(400)
+                .then(function (res) {
                 expect(res.body.message).to.equal('Invalid Request');
             });
         });
         xit('DELETE / responds with status 405 - Invalid Method', function () {
-            return request["delete"]('/api/surgeries').expect(405).then(function (res) {
+            return request["delete"]('/api/surgeries')
+                .expect(405)
+                .then(function (res) {
                 expect(res.body.msg).to.equal('Method Not Allowed');
             });
         });
--- spec/app.spec.ts
+++ spec/app.spec.ts
@@ -4,16 +4,13 @@ const { expect } = require('chai');
 const supertest = require('supertest');
 const connection = require('../db/connection.js');
 const appTest = require('../app');
-import { Request, Response } from 'express';
+// import { Request, Response } from 'express';
 
 const request = supertest(appTest);
 // console.log(appTest)
 
 describe('/', () => {
-  beforeEach(() => {
-    console.log(appTest.listeners());
-    return connection.seed.run()
-  });
+  beforeEach(() => connection.seed.run());
   after(() => connection.destroy());
   describe('/gps', () => {
 
@@ -21,7 +18,7 @@ describe('/', () => {
   describe('/patients', () => {
 
   });
-  describe.only('/surgeries', () => {
+  describe('/surgeries', () => {
     it('GET 200 returns a list of all the surgeries', () => {
       return request.get('/api/surgeries').expect(200).then((res: any) => {
         expect(res.body.surgeries[0]).to.contain.keys('surgery_id', 'surgery_name', 'surgery_username', 'surgery_password', 'surgery_address')
@@ -29,18 +26,30 @@ describe('/', () => {
     });
     it('POST 201 return a posted surgery', () => {
       const newSurgery = { surgery_name: 'New Surgery', surgery_username: 'newSurgery', surgery_password: 'newsurgery21', surgery_address: '121 new suregery street m21 3th' };
-      return request.post('/api/surgeries').send(newSurgery).expect(201).then((res: any) => {
+      console.log(appTest.listeners(), '<-- listeners during 2');
+      return request
+      .post('/api/surgeries')
+      .send(newSurgery)
+      .expect(201)
+      .then((res: any) => {
         expect(res.body.surgery).to.contain.keys('surgery_id', 'surgery_name', 'surgery_username', 'surgery_password', 'surgery_address')
       })
     })
     it('POST / responds with status 400 - Invalid Body', () => {
       const newSurgery = { surgery_username: 'newSurgery', surgery_password: 'newsurgery21', surgery_address: '121 new suregery street m21 3th' };
-      return request.post('/api/surgeries').send(newSurgery).expect(400).then((res: any) => {
+      return request
+      .post('/api/surgeries')
+      .send(newSurgery)
+      .expect(400)
+      .then((res: any) => {
         expect(res.body.message).to.equal('Invalid Request')
       })
     });
     xit('DELETE / responds with status 405 - Invalid Method', () => {
-      return request.delete('/api/surgeries').expect(405).then((res: any) => {
+      return request
+      .delete('/api/surgeries')
+      .expect(405)
+      .then((res: any) => {
         expect(res.body.msg).to.equal('Method Not Allowed')
       })
     });
