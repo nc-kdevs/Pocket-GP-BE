fe096be4380fa06fe60a1edf7e2c39305638ca94
Viktorija Cumacenko <vikuce@gmail.com>
2019-04-05 11:12:49 +0100

    add encryption func

--- security/decryption.js
+++ security/decryption.js
@@ -4,14 +4,20 @@ let iv = crypto.randomBytes(16)
 
 let decKey = '12345678123456781234567812345678';
 
-export let decipher = crypto.createDecipheriv('aes-256-cbc', decKey, iv);
-export let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
-decrypted += decipher.final('utf-8');
+// export let decipher = crypto.createDecipheriv('aes-256-cbc', decKey, iv);
+// export let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
+// decrypted += decipher.final('utf-8');
 
-decryptData = (data) => {
-  const encKey = '12345678123456781234567812345678';
-  let decipher = crypto.createDecipheriv('aes-256-cbc', decKey, iv);
-  let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
-  decrypted += decipher.final('utf-8');
-    return decrypted
-}
+// decryptData = (data) => {
+//   const encKey = '12345678123456781234567812345678';
+//   let decipher = crypto.createDecipheriv('aes-256-cbc', decKey, iv);
+//   let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
+//   decrypted += decipher.final('utf-8');
+//     return decrypted
+// }
+
+// const decrypt=(encryptedObj) => {
+//   const array = Object.entries(encryptedObj);
+//   const newArray.map()
+//   let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+// }
--- security/encryption.js
+++ security/encryption.js
@@ -4,23 +4,41 @@ let iv = crypto.randomBytes(16)
 
 const encKey = '12345678123456781234567812345678';
 
-export let cipher = crypto.createCipheriv('aes-256-cbc', encKey, iv);
-export let encrypted = cipher.update(dataToEnc, 'utf-8', 'hex');
-encrypted += cipher.final('hex');
+// export let cipher = crypto.createCipheriv('aes-256-cbc', encKey, iv);
+// export let encrypted = cipher.update(dataToEnc, 'utf-8', 'hex');
+// encrypted += cipher.final('hex');
 
-export const encryptDataObj = (dataObj) => {
-  const {dataObj} = dataToEnc  
-  const encKey = '12345678123456781234567812345678';
-  let cipher = crypto.createCipheriv('aes-256-cbc', encKey, iv);
-  let encrypted = cipher.update(dataToEnc, 'utf-8', 'hex');
-  encrypted += cipher.final('hex');
-    return encrypted
-}
+// export const encryptDataObj = (dataObj) => {
+//   const {dataObj} = dataToEnc  
+//   const encKey = '12345678123456781234567812345678';
+//   let cipher = crypto.createCipheriv('aes-256-cbc', encKey, iv);
+//   let encrypted = cipher.update(dataToEnc, 'utf-8', 'hex');
+//   encrypted += cipher.final('hex');
+//     return encrypted
+// }
 
-export const encryptData = (data) => {
-  const encKey = '12345678123456781234567812345678';
-  let cipher = crypto.createCipheriv('aes-256-cbc', encKey, iv);
-  let encrypted = cipher.update(dataToEnc, 'utf-8', 'hex');
-  encrypted += cipher.final('hex');
-    return encrypted
+// export const encryptData = (data) => {
+//   const encKey = '12345678123456781234567812345678';
+//   let cipher = crypto.createCipheriv('aes-256-cbc', encKey, iv);
+//   let encrypted = cipher.update(dataToEnc, 'utf-8', 'hex');
+//   encrypted += cipher.final('hex');
+//     return encrypted
+// }
+
+exports.encrypt = (obj) => {
+  const array = Object.entries(obj);
+  const newArray = array.map(([key, value]) => {
+    if (typeof value === 'string') {
+      let cipher = crypto.createCipheriv('aes-256-cbc', encKey, iv);
+      let encrypted = cipher.update(value, 'utf-8', 'hex');
+      encrypted += cipher.final('hex');
+      return [key, encrypted]
+    }
+    else return [key, value];
+  })
+  const encryptedObj = {};
+  newArray.forEach(pair => {
+    encryptedObj[pair[0]] = pair[1];
+  })
+  return encryptedObj;
 }
